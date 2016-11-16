b771b1dafbbe511bdafdd3a0a88b22288c93c5c9
Julian NG <juliannieb@gmail.com>
2016-11-15 20:47:18 -0600

    Fix test cases and add input parser for training the data

---
+++ perceptron.py
@@ -0,0 +1,33 @@
+from random import choice
+from numpy import array, dot, random
+import sys
+
+def unit_step(x):
+    if x < 0:
+        return 0
+    else:
+        return 1
+
+training_data = []
+
+for line in sys.stdin:
+    line = line.strip("\n")
+    line = line.split(' ')
+    data = (array([int(i) for i in line[:len(line)-1]]), int(line[len(line)-1]))
+    training_data.append(data)
+
+print(training_data)
+
+w = random.rand(3)
+eta = 0.2
+n = 100
+
+for i in xrange(n):
+    x, expected = get_random_position(training_data)
+    result = dot(w, x)
+    error = expected - unit_step(result)
+    w += eta * error * x
+
+for x, _ in training_data:
+    result = dot(x, w)
+    print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
